717e7fe34c317fc1c765aeef910faa3076110b75
Raúl Torres Alarcón <raultorres2603@gmail.com>
2022-11-18 12:08:16 +0100

    First Commit

---
+++ src/components/MainMenu.js
@@ -0,0 +1,75 @@
+import { Papa } from "papaparse";
+import { useState } from "react";
+import { useCSVReader } from "react-papaparse";
+
+function MainMenu() {
+  const [jsonFile, setJsonFile] = useState(null);
+  const { CSVReader } = useCSVReader();
+
+  function handleFile(ev) {
+    const result = Papa.parse(ev.target);
+    console.log(result);
+  }
+
+  return (
+    <div className="col-md-12">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="titulo text-center display-1">Cargar CSV</div>
+          <hr />
+          <div className="row">
+            <CSVReader
+              onUploadAccepted={(results) => {
+                console.log("---------------------------");
+                console.log(results.data);
+                setJsonFile(results.data);
+                console.log("---------------------------");
+              }}
+              on
+            >
+              {({
+                getRootProps,
+                acceptedFile,
+                ProgressBar,
+                getRemoveFileProps,
+              }) => (
+                <>
+                  <div className="row">
+                    <div className="d-grid gap-2">
+                      <button
+                        type="button"
+                        className="btn btn-info"
+                        {...getRootProps()}
+                      >
+                        Browse file
+                      </button>
+                    </div>
+
+                    <div>{acceptedFile && acceptedFile.name}</div>
+                  </div>
+                  <ProgressBar />
+                </>
+              )}
+            </CSVReader>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="row">
+            <div
+              className="inform border border-dark border-5"
+              style={{ height: `98vh` }}
+            >
+              <p className="display-1">Resultado</p>
+              <hr />
+              <div className="row">
+                <div className="result">{JSON.stringify(jsonFile)}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainMenu;
